c9529f0bd7381b51ae78a16cc51fc46e0f00bd0f
sergi.ortiz <sergi.ortiz.rodriguez@gmail.com>
2018-03-24 10:30:10 +0100

    Small refactors in code
    New internal methods created for more robustness. Also small changes in code to make it cleaner

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import hashlib
 import json
 from time import time
 from urllib.parse import urlparse
-from uuid import uuid4
-from flask import Flask, jsonify, request
+# from uuid import uuid4
+# from flask import Flask, jsonify, request
 
 token = os.environ['TOKEN']
 
@@ -27,7 +27,17 @@ logger.info("IT'S SHOWTIME")
 protocol_end_point = 'http://'
 
 
-class Blockchain:
+class BlockChain:
+
+    """
+    More info about this class within
+
+    https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+    https://github.com/dvf/blockchain/blob/master/blockchain.py
+
+    Warning: Minor changes have been applied to the original class
+    """
+
     def __init__(self):
         self.current_transactions = []
         self.chain = []
@@ -36,24 +46,10 @@ class Blockchain:
         # Create the genesis block
         self.new_block(previous_hash='1', proof=100)
 
-    def register_node(self, address):
-        """
-        Add a new node to the list of nodes
-        :param address: Address of node. Eg. 'http://192.168.0.5:5000'
-        """
-
-        parsed_url = urlparse(address)
-        if parsed_url.netloc:
-            self.nodes.add(parsed_url.netloc)
-        elif parsed_url.path:
-            # Accepts an URL without scheme like '192.168.0.5:5000'.
-            self.nodes.add(parsed_url.path)
-        else:
-            raise ValueError('Invalid URL')
-
-    def valid_chain(self, chain):
+    def _valid_chain(self, chain):
         """
         Determine if a given blockchain is valid
+
         :param chain: A blockchain
         :return: True if valid, False if not
         """
@@ -63,15 +59,18 @@ class Blockchain:
 
         while current_index < len(chain):
             block = chain[current_index]
-            print(f'{last_block}')
-            print(f'{block}')
-            print("\n-----------\n")
+
+            logger.debug('Last block: {}'.format(last_block))
+            logger.debug('Block: {}'.format(block))
+
             # Check that the hash of the block is correct
-            if block['previous_hash'] != self.hash(last_block):
+            if block['previous_hash'] != self._hash(last_block):
                 return False
 
             # Check that the Proof of Work is correct
-            if not self.valid_proof(last_block['proof'], block['proof'], last_block['previous_hash']):
+            if not self._valid_proof(last_block['proof'],
+                                    block['proof'],
+                                    last_block['previous_hash']):
                 return False
 
             last_block = block
@@ -79,31 +78,52 @@ class Blockchain:
 
         return True
 
+    def register_node(self, address):
+        """
+        Add a new node to the list of nodes
+
+        :param address: <str> Address of node. Eg. 'http://192.168.0.5:5000'
+        """
+
+        parsed_url = urlparse(address)
+
+        if parsed_url.netloc:
+            self.nodes.add(parsed_url.netloc)
+        elif parsed_url.path:
+            # Accepts an URL without scheme like '192.168.0.5:5000'.
+            self.nodes.add(parsed_url.path)
+        else:
+            raise ValueError('Invalid URL')
+
     def resolve_conflicts(self):
         """
         This is our consensus algorithm, it resolves conflicts
         by replacing our chain with the longest one in the network.
+
         :return: True if our chain was replaced, False if not
         """
 
-        neighbours = self.nodes
         new_chain = None
 
         # We're only looking for chains longer than ours
         max_length = len(self.chain)
 
         # Grab and verify the chains from all the nodes in our network
-        for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+        for node in self.nodes:
 
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
+            # make request and retrieve length and chain
+            length, chain = self._make_request(node)
 
-                # Check if the length is longer and the chain is valid
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+            # if the request failed then continue
+            if not length or not chain:
+                continue
+
+            # Check if the length is longer and the chain is valid
+            if length > max_length and self._valid_chain(chain):
+                max_length = length
+                new_chain = chain
+            else:
+                continue
 
         # Replace our chain if we discovered a new, valid chain longer than ours
         if new_chain:
@@ -115,8 +135,9 @@ class Blockchain:
     def new_block(self, proof, previous_hash):
         """
         Create a new Block in the Blockchain
-        :param proof: The proof given by the Proof of Work algorithm
-        :param previous_hash: Hash of previous Block
+
+        :param proof: <str> The proof given by the Proof of Work algorithm
+        :param previous_hash: <str> Hash of previous Block
         :return: New Block
         """
 
@@ -125,21 +146,22 @@ class Blockchain:
             'timestamp': time(),
             'transactions': self.current_transactions,
             'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'previous_hash': previous_hash or self._hash(self.chain[-1]),
         }
 
         # Reset the current list of transactions
         self.current_transactions = []
 
         self.chain.append(block)
+
         return block
 
     def new_transaction(self, sender, recipient, amount):
         """
         Creates a new transaction to go into the next mined Block
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
         :return: The index of the Block that will hold this transaction
         """
         self.current_transactions.append({
@@ -149,21 +171,6 @@ class Blockchain:
         })
 
         return self.last_block['index'] + 1
-
-    @property
-    def last_block(self):
-        return self.chain[-1]
-
-    @staticmethod
-    def hash(block):
-        """
-        Creates a SHA-256 hash of a Block
-        :param block: Block
-        """
-
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
 
     def proof_of_work(self, last_block):
         """
@@ -176,26 +183,72 @@ class Blockchain:
         """
 
         last_proof = last_block['proof']
-        last_hash = self.hash(last_block)
+        last_hash = self._hash(last_block)
 
         proof = 0
-        while self.valid_proof(last_proof, proof, last_hash) is False:
+        while self._valid_proof(last_proof, proof, last_hash) is False:
             proof += 1
 
         return proof
 
+    @property
+    def last_block(self):
+        return self.chain[-1]
+
     @staticmethod
-    def valid_proof(last_proof, proof, last_hash):
+    def _hash(block):
         """
-        Validates the Proof
+        Creates a SHA-256 hash of a Block
+
+        :param block: <dict> Block
+        """
+
+        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
+        block_string = json.dumps(block, sort_keys=True).encode()
+
+        return hashlib.sha256(block_string).hexdigest()
+
+    @staticmethod
+    def _valid_proof(last_proof, proof, last_hash):
+        """
+        Validates the Proof of work
+
         :param last_proof: <int> Previous Proof
         :param proof: <int> Current Proof
         :param last_hash: <str> The hash of the Previous Block
         :return: <bool> True if correct, False if not.
         """
 
-        guess = f'{last_proof}{proof}{last_hash}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
+        guess = '{lp}{p}{lh}'.format(lp=last_proof,
+                                     p=proof,
+                                     lh=last_hash)
 
+        guess_hash = hashlib.sha256(guess.encode()).hexdigest()
 
         return guess_hash[:4] == "0000"
+
+    @staticmethod
+    def _make_request(node):
+        """
+        1. Make query to end point
+        2. Validate the response status_code
+
+        :param node:
+        :return:
+        """
+
+        end_point = '{p}{node}/chain'.format(p=protocol_end_point,
+                                             node=node)
+
+        response = requests.get(end_point)
+
+        # This part following shall be a separated method
+        if response.status_code == 200:
+            length = response.json()['length']
+            chain = response.json()['chain']
+        else:
+            length = None
+            chain = None
+            logger.error('API response failed with status code: {}'.format(response.status_code))
+
+        return length, chain
